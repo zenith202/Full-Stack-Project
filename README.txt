0e7e02b34ebc557539f9cc0babb9635cf3c5b8e1
Ziyi Liew <zliew@utexas.edu>
2020-11-21 10:58:45 +0800

    adding initial files

---
+++ README.txt
@@ -0,0 +1,4 @@
+Name: Zi Yi Liew
+EID: ZL7279
+Bitbucketid: zliew
+applicationURL:
---
+++ application.py
@@ -0,0 +1,47 @@
+import json
+
+from flask import request
+from flask import Flask, render_template
+
+
+application = Flask(__name__)
+app = application
+all_pools = []
+
+@app.route("/static/search_libraries", methods=['POST'])
+def get_libraries():
+
+    # Assignment 4: Insert Pool into database.
+    # Extract all the form fields
+    pool_name = request.form['district']
+    computers = request.form['computers']
+    print("District:" + pool_name)
+    print("Status:" + computers)
+
+    # Insert into database.
+
+
+    return render_template('pool_added.html')
+
+
+@app.route("/pools")
+def get_pools():
+    # Assignment 4: 
+    # Query the database to pull all the pools
+    # Sample pool -- Delete this from final output.
+    pool = {}
+    pool['Name'] = 'Barton Springs'
+    pool['Timings'] = 'Friday - Sunday'
+    pool['Status'] = 'Open'
+    all_pools.append(pool)
+    return json.dumps(all_pools)
+
+
+@app.route("/")
+def pool_info_website():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0')
